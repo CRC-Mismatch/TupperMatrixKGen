d37870f1922adcade2e758c3532c77f8d1b3f97f
CRC-Mismatch <kevin.mian@gmail.com>
2015-07-27 21:58:02 -0300

    Newer

--- TupperKMatrixGen/MainWindow.xaml.cs
+++ TupperKMatrixGen/MainWindow.xaml.cs
@@ -22,6 +22,7 @@ namespace TupperKMatrixGen
     {
         protected double drawGrid = 1;
         protected bool? wasFirstBlack = false;
+        protected bool initialized = false;
         public MainWindow()
         {
             InitializeComponent();
@@ -36,15 +37,22 @@ namespace TupperKMatrixGen
 
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
+            if (!initialized)
+            {
+                initialized = true;
+                return;
+            }
             drawGrid =  1;
             for (int i = 0; i < 106; i++)
             {
                 for (int j = 0; j < 17; j++)
                 {
                     Rectangle r = (Rectangle)FindName("Rect" + i + "x" + j);
-                    r.StrokeThickness = (r.StrokeThickness == drawGrid) ? drawGrid : 4;
+                    r.StrokeThickness = (r.StrokeThickness == 0) ? drawGrid : 4;
                 }
             }
+            Border1.Height = 1;
+            Border2.Width = 1;
         }
 
         private void Rect0x0_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
@@ -81,9 +89,12 @@ namespace TupperKMatrixGen
                 for (int j = 0; j < 17; j++)
                 {
                     Rectangle r = (Rectangle)FindName("Rect" + i + "x" + j);
-                    r.StrokeThickness = (r.StrokeThickness == drawGrid) ? drawGrid : 4;
+                    r.StrokeThickness = (r.StrokeThickness == 1) ? drawGrid : 4;
                 }
             }
+            initialized = true;
+            Border1.Height = 0;
+            Border2.Width = 0;
         }
         private void doIt_JustDoIt(object sender, RoutedEventArgs e)
         {
